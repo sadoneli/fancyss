0d4163662633bb74715f4d537191d9a645163423
Zed <zed@zedd.dev>
2020-04-23 14:19:54 +0800

    hnd: 国内优先dns模式下会加入ISP下发的dnsv6地址。未反向测试

--- fancyss_hnd/config.json.js
+++ fancyss_hnd/config.json.js
@@ -1,8 +1,8 @@
 {
-"build_date":"2020-04-05_21:15:28",
+"build_date":"2020-04-18_11:43:27",
 "description":"科学上网",
 "home_url":"Module_shadowsocks.asp",
-"md5":"f35c6091a086d0b27bf1c197771e9382",
+"md5":"89f0e3e00a19751de0c53e7d014dcd65",
 "name":"shadowsocks",
 "tar_url": "https://raw.githubusercontent.com/hq450/fancyss/master/fancyss_hnd/shadowsocks.tar.gz", 
 "title":"科学上网",
