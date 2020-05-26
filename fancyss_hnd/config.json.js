3894ef8f5841f10848aa56ea6f186731977ae8cb
Zed <zed@zedd.dev>
2020-05-27 06:36:22 +0800

    hnd: fix crond parse error

--- fancyss_hnd/config.json.js
+++ fancyss_hnd/config.json.js
@@ -1,8 +1,8 @@
 {
-"build_date":"2020-04-18_11:43:27",
+"build_date":"2020-05-27_06:35:27",
 "description":"科学上网",
 "home_url":"Module_shadowsocks.asp",
-"md5":"89f0e3e00a19751de0c53e7d014dcd65",
+"md5":"183f2117a2f5e0e3081178cf1547c712",
 "name":"shadowsocks",
 "tar_url": "https://raw.githubusercontent.com/hq450/fancyss/master/fancyss_hnd/shadowsocks.tar.gz", 
 "title":"科学上网",
